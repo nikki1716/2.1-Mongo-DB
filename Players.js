97d137df07d16352b3c71911f589035d4364000a
nikki1716 <137245130+nikki1716@users.noreply.github.com>
2023-06-24 13:12:49 +0300

    Create Players.js

---
+++ Players.js
@@ -0,0 +1,33 @@
+db.players.insertMany([
+  {
+    "_id": 1,
+    "name": "Aaron Rodgers",
+    "position": "QB",
+    "rushing_yards": 100,
+    "touchdown_passes": 30,
+    "sacks": 10,
+    "made_field_goals": 20,
+    "missed_field_goals": 5
+  },
+  {
+    "_id": 2,
+    "name": "Davante Adams",
+    "position": "WR",
+    "rushing_yards": 0,
+    "touchdown_passes": 15,
+    "sacks": 0,
+    "made_field_goals": 0,
+    "missed_field_goals": 0
+  },
+  {
+    "_id": 3,
+    "name": "Jaire Alexander",
+    "position": "CB",
+    "rushing_yards": 0,
+    "touchdown_passes": 0,
+    "sacks": 15,
+    "made_field_goals": 0,
+    "missed_field_goals": 0
+  },
+  ...
+]);
